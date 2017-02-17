fa3a1771369f20ee070171a32389456152b3500e
vsvictor <dvictor74@gmail.com>
2017-02-17 21:12:01 +0200

    Fix Request class

--- revsdk/src/main/java/com/rev/revsdk/RevSDK.java
+++ revsdk/src/main/java/com/rev/revsdk/RevSDK.java
@@ -2,17 +2,11 @@ package com.rev.revsdk;
 
 import android.util.Log;
 
-import com.rev.revsdk.config.Config;
-import com.rev.revsdk.config.ListString;
-import com.rev.revsdk.interseptor.RequesCreator;
-import com.rev.revsdk.utils.Tag;
+import com.rev.revsdk.interseptor.RequestCreator;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
-import okhttp3.Headers;
-import okhttp3.HttpUrl;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -57,9 +51,7 @@ public class RevSDK {
                 boolean isSystem = (original.tag() != null);
                 Log.i(TAG, "is System?"+String.valueOf(isSystem)+" ,Intercepted: \n"+original.toString());
                 //if(!isSystem)
-                result = processingRequest(original);
-                Log.i(TAG, "Intercepted: \n"+result.toString());
-
+                    result = processingRequest(original);
                 //else result = original;
                 return chain.proceed(result);
             }
@@ -70,7 +62,7 @@ public class RevSDK {
 
     private static Request processingRequest(Request original){
         Request result = null;
-        RequesCreator creator = new RequesCreator(RevApplication.getInstance().getConfig());
+        RequestCreator creator = new RequestCreator(RevApplication.getInstance().getConfig());
         Log.i(TAG, "Intercepted: \n"+original.toString());
         return creator.create(original);
     }

8c6ff6e324d0d87dd6986394e9ec0ece3399297e
victor <dvictor74@gmail.com>
2017-04-02 02:58:39 +0300

    Compatibility - Retrofin2, Picasso & Volley

--- RevWeather/src/main/java/com/rev/weather/picasso/DataAdapter.java
+++ RevWeather/src/main/java/com/rev/weather/picasso/DataAdapter.java
@@ -2,6 +2,9 @@ package com.rev.weather.picasso;
 
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -42,13 +45,76 @@ public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(ViewHolder viewHolder, final int i) {
 
         viewHolder.tv_android.setText(android_versions.get(i).getAndroid_version_name());
         String sLoad = android_versions.get(i).getAndroid_image_url();
         picasso.load(sLoad).resize(120, 60).into(viewHolder.img_android);
-        //picasso.load(sLoad).into(viewHolder.img_android);
+
         if (listener != null) listener.onPicassoLoad(sLoad);
+        viewHolder.cvCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sText = null;
+                switch (i) {
+                    case 0: {
+                        sText = context.getResources().getString(R.string.donut);
+                        break;
+                    }
+                    case 1: {
+                        sText = context.getResources().getString(R.string.eclair);
+                        break;
+                    }
+                    case 2: {
+                        sText = context.getResources().getString(R.string.froyo);
+                        break;
+                    }
+                    case 3: {
+                        sText = context.getResources().getString(R.string.ginger);
+                        break;
+                    }
+                    case 4: {
+                        sText = context.getResources().getString(R.string.honey);
+                        break;
+                    }
+                    case 5: {
+                        sText = context.getResources().getString(R.string.icecream);
+                        break;
+                    }
+                    case 6: {
+                        sText = context.getResources().getString(R.string.jellybean);
+                        break;
+                    }
+                    case 7: {
+                        sText = context.getResources().getString(R.string.kitkat);
+                        break;
+                    }
+                    case 8: {
+                        sText = context.getResources().getString(R.string.lollipop);
+                        break;
+                    }
+                    case 9: {
+                        sText = context.getResources().getString(R.string.marshmallow);
+                        break;
+                    }
+                }
+                AlertDialog dialog;
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle(android_versions.get(i).getAndroid_version_name());
+                View view = LayoutInflater.from(context).inflate(R.layout.dialog, null, false);
+                TextView tv = (TextView) view.findViewById(R.id.tvText);
+                tv.setText(sText);
+                builder.setView(view);
+                builder.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                dialog = builder.create();
+                dialog.show();
+            }
+        });
     }
 
     @Override
@@ -57,11 +123,13 @@ public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder> {
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView tv_android;
-        ImageView img_android;
+        final CardView cvCard;
+        final TextView tv_android;
+        final ImageView img_android;
 
         public ViewHolder(View view) {
             super(view);
+            cvCard = (CardView) view.findViewById(R.id.cvCard);
             tv_android = (TextView) view.findViewById(R.id.tv_android);
             img_android = (ImageView) view.findViewById(R.id.img_android);
         }

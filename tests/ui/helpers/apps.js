cd93c903e7bb45ad2411af480a29a900649d1268
oleg <oleg@MacBook-Pro-oleg.local>
2017-08-25 21:18:07 +0300

    change apps.js file

--- tests/ui/helpers/apps.js
+++ tests/ui/helpers/apps.js
@@ -1,8 +1,8 @@
 
-var currentFolder = __dirname.replace(/revsw\-sdk\-android\/.*/g, '');
+//var currentFolder = __dirname.replace(/revsw\-sdk\-android\/.*/g, '');
 console.log('NEW PUTH ==>> ', __dirname);
 ///var/lib/jenkins/workspace/appium-tests/NuubitTester/build/outputs/apk/NuubitTester-release-unsigned.apk
-exports.androidTester = "/var/lib/jenkins/workspace/appium-tests/NuubitTester/build/outputs/apk/NuubitTester-release-unsigned.apk";
+//exports.androidTester = "/var/lib/jenkins/workspace/appium-tests/NuubitTester/build/outputs/apk/NuubitTester-release-unsigned.apk";
 //exports.androidTester = currentFolder + "apks/NuubitTester-debug.apk";
 //exports.androidTester = currentFolder + "apps-android/apks/NuubitTester-debug.apk";
 //exports.androidTesterInvalidSDKkey = currentFolder + "apps-android/apks/NuubitTesterInvalidSDKkey.apk";

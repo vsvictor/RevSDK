dbe4fb1a10a6f251bb00262cd6bea7d9304982e7
IlarionHalushka <hilarion.galushka@gmail.com>
2017-09-27 12:03:33 +0300

    adding of apk file that is not crashing

--- tests/ui/helpers/apps.js
+++ tests/ui/helpers/apps.js
@@ -4,8 +4,8 @@ console.log('NEW PUTH ==>> ', __dirname);
 //--  /var/lib/jenkins/workspace/appium-tests/tests/ui/helpers
 
 ///var/lib/jenkins/workspace/appium-tests/NuubitTester/build/outputs/apk/NuubitTester-release-unsigned.apk
-exports.androidTester = "/var/lib/jenkins/workspace/appium-tests/NuubitTester/build/outputs/apk/NuubitTester-debug.apk";
-//exports.androidTester = currentFolder + "/apks/NuubitTester-debug.apk";
+//exports.androidTester = "/var/lib/jenkins/workspace/appium-tests/NuubitTester/build/outputs/apk/NuubitTester-debug.apk";
+exports.androidTester = currentFolder + "/apks/NuubitTester-debug.apk";
 //exports.androidTester = currentFolder + "apps-android/apks/NuubitTester-debug.apk";
 exports.androidTesterInvalidSDKkey = currentFolder + "/apks/NuubitTesterInvalidSDKKey.apk";
 exports.androidTesterSameSDKKey = currentFolder + "/apks/NuubitTesterSameSDKKey.apk";
